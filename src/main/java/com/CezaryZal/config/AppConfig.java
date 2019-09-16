7e5261d8fd7be505568b0c1dfdcde6a302faeee2
CezaryZal <czarek0821@gmail.com>
2019-09-16 17:45:48 +0200

    config hibernate

--- src/main/java/com/CezaryZal/config/AppConfig.java
+++ src/main/java/com/CezaryZal/config/AppConfig.java
@@ -1,16 +1,16 @@
 package com.CezaryZal.config;
 
-//import com.mchange.v2.c3p0.ComboPooledDataSource;
-//import org.hibernate.SessionFactory;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-//import org.springframework.orm.hibernate5.HibernateTransactionManager;
-//import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,9 +22,9 @@ import java.util.logging.Logger;
 
 @Configuration
 @EnableWebMvc
-//@EnableTransactionManagement
+@EnableTransactionManagement
 @ComponentScan("com.CezaryZal")
-//@PropertySource("classpath:persistence-mysql.properties")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -42,83 +42,73 @@ public class AppConfig implements WebMvcConfigurer {
 
     }
 
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactory(){
-//
-//        // create session factory
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//
-//        // set the properties
-//        sessionFactory.setDataSource(dataSource());
-////        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
-////        sessionFactory.setHibernateProperties(getHibernateProperties());
-//
-//        return sessionFactory;
-//    }
-//
-//    @Bean
-//    @Autowired
-//    public HibernateTransactionManager transactionManager (SessionFactory sessionFactory){
-//
-//        // set transaction manager based on session factory
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-//        transactionManager.setSessionFactory(sessionFactory);
-//
-//        return transactionManager;
-//    }
-//
-//    @Bean
-//    public DataSource dataSource() {
-//
-//        // create connection pool
-//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-//
-//        // set the jdbc driver
-//        try {
-//            dataSource.setDriverClass(environment.getProperty("jdbc.driver"));
-//            System.out.println("wewnątrz dataSource");
-//        }
-//        catch (PropertyVetoException error) {
-//            throw new RuntimeException(error);
-//        }
-//
-//        //log the connection props
-//        //just to make sure we are REALLY reading data from properties file
-//        logger.info("jdbc.url=" + environment.getProperty("jdbc.url"));
-//        logger.info("jdbc.user=" + environment.getProperty("jdbc.user"));
-//
-//        // set database connection props
-//        dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
-//        dataSource.setUser(environment.getProperty("jdbc.user"));
-//        dataSource.setPassword(environment.getProperty("jdbc.password"));
-//
-//        // set connection pool props
-//        dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
-//        dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
-//        dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
-//        dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
-//
-//        return dataSource;
-//    }
-//
-//    //helper method - read environment property and convert to int
-//    private int getIntProperty (String propName){
-//        String propVal = environment.getProperty(propName);
-//        int intPropVal = Integer.parseInt(propVal);
-//
-//        return intPropVal;
-//    }
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
 
-//    private Properties getHibernateProperties() {
-//
-//        // set hibernate properties
-//        Properties props = new Properties();
-//
-//        props.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-//        props.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
-//
-//        return props;
-//    }
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager (SessionFactory sessionFactory){
+
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        }
+        catch (PropertyVetoException error) {
+            throw new RuntimeException(error);
+        }
+
+        logger.info("jdbc.url=" + environment.getProperty("jdbc.url"));
+        logger.info("jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        // set database connection props
+        dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUser(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        // set connection pool props
+        dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return dataSource;
+    }
+
+    private int getIntProperty (String propName){
+        String propVal = environment.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+    }
+
+    private Properties getHibernateProperties() {
+
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
 
 
 }
--- src/main/java/com/CezaryZal/config/AppInitializer.java
+++ src/main/java/com/CezaryZal/config/AppInitializer.java
@@ -1,7 +1,5 @@
 package com.CezaryZal.config;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Driver;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -9,8 +7,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class AppInitializer implements WebApplicationInitializer {
     @Override
