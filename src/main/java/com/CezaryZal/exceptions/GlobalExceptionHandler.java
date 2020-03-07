cf0a600688b4186b290fe34284113a8db2731242
CezaryZal <czarek0821@gmail.com>
2020-03-07 20:26:42 +0100

    Create GlobalExceptionHandler class to handel all internal Exceptions

---
+++ src/main/java/com/CezaryZal/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.CezaryZal.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler({
+            InvalidInputException.class,
+            InvalidSymbolsException.class,
+            InvalidValueOfDimensionException.class,
+            InvalidValueOfItException.class})
+    protected ResponseEntity<Object> handleValidationLoginAndPasswordExceptions(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
