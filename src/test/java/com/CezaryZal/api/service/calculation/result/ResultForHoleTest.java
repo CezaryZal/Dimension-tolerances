aae8da7dfe8cc6baf869de5c1212c8360e91cd60
CezaryZal <czarek0821@gmail.com>
2020-03-12 19:54:20 +0100

    Code optimization in classes of ResultFor...Test

--- src/test/java/com/CezaryZal/api/service/calculation/result/ResultForHoleTest.java
+++ src/test/java/com/CezaryZal/api/service/calculation/result/ResultForHoleTest.java
@@ -12,8 +12,6 @@ import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResultForHoleTest {
@@ -30,19 +28,16 @@ public class ResultForHoleTest {
 
     //100J5 : 5 (lower), 20 (upper)
     @Test
-    public void calculate_should_testing_for_symbol_over_H(){
-        given(resultForHole.calculateForSymbolOverH(100, 20, 15))
-                .willReturn(new DimensionDtoImpl(100, 5, 20));
-
-
-        ParsedInputDimension parsedInputDimensionFrom100J5 =
-                new ParsedInputDimension(100, 'J', 5);
-        ValuesToDimensionDto valuesToDimensionDtoFrom100J5 =
-                new ValuesToDimensionDto(20, 15);
-        DimensionDto dimensionDtoExpectedFrom100J5 =
-                resultForHole.calculate(valuesToDimensionDtoFrom100J5, parsedInputDimensionFrom100J5);
-
+    public void calculate_should_testing_for_symbol_over_H() {
         DimensionDtoImpl dimensionDtoImpFrom100K6 = new DimensionDtoImpl(100, 5, 20);
+
+        given(resultForHole.calculateForSymbolOverH(100, 20, 15))
+                .willReturn(dimensionDtoImpFrom100K6);
+
+        DimensionDto dimensionDtoExpectedFrom100J5 =
+                resultForHole.calculate(
+                        new ValuesToDimensionDto(20, 15),
+                        new ParsedInputDimension(100, 'J', 5));
 
         Assert.assertEquals(dimensionDtoExpectedFrom100J5, dimensionDtoImpFrom100K6);
         Assert.assertNotEquals(dimensionDtoExpectedFrom100J5, testClassOfDimensionDtoImp);
@@ -52,33 +47,28 @@ public class ResultForHoleTest {
     //200F7 : 50 (lower), 96 (upper)
     @Test
     public void calculate_should_testing_for_symbol_below_or_equal_H() {
-        given(resultForHole.calculateForSymbolBelowOrEqualH(30, 0, 33))
-                .willReturn(new DimensionDtoImpl(30, 0, 33));
-
-        ParsedInputDimension parsedInputDimensionFrom30H8 =
-                new ParsedInputDimension(30, 'H', 8);
-        ValuesToDimensionDto valuesToDimensionDtoFrom30H8 =
-                new ValuesToDimensionDto(0, 33);
-        DimensionDto dimensionDtoExpectedFrom30H8 =
-                resultForHole.calculate(valuesToDimensionDtoFrom30H8, parsedInputDimensionFrom30H8);
-
         DimensionDtoImpl dimensionDtoImpFrom30H8 = new DimensionDtoImpl(30, 0, 33);
+        DimensionDtoImpl dimensionDtoImpFrom200F7 = new DimensionDtoImpl(200, 50, 96);
+
+        given(resultForHole.calculateForSymbolBelowOrEqualH(30, 0, 33))
+                .willReturn(dimensionDtoImpFrom30H8);
+
+        DimensionDto dimensionDtoExpectedFrom30H8 =
+                resultForHole.calculate(
+                        new ValuesToDimensionDto(0, 33),
+                        new ParsedInputDimension(30, 'H', 8));
 
         Assert.assertEquals(dimensionDtoExpectedFrom30H8, dimensionDtoImpFrom30H8);
         Assert.assertNotEquals(dimensionDtoExpectedFrom30H8, testClassOfDimensionDtoImp);
 
 
-        given(resultForHole.calculateForSymbolBelowOrEqualH(30, 50, 46))
-                .willReturn(new DimensionDtoImpl(200, 50, 96));
+        given(resultForHole.calculateForSymbolBelowOrEqualH(200, 50, 46))
+                .willReturn(dimensionDtoImpFrom200F7);
 
-        ParsedInputDimension parsedInputDimensionFrom200F7 =
-                new ParsedInputDimension(200, 'F', 7);
-        ValuesToDimensionDto valuesToDimensionDtoFrom200F7 =
-                new ValuesToDimensionDto(50, 46);
         DimensionDto dimensionDtoExpectedFrom200F7 =
-                resultForHole.calculate(valuesToDimensionDtoFrom200F7, parsedInputDimensionFrom200F7);
-
-        DimensionDtoImpl dimensionDtoImpFrom200F7 = new DimensionDtoImpl(200, 50, 96);
+                resultForHole.calculate(
+                        new ValuesToDimensionDto(50, 46),
+                        new ParsedInputDimension(200, 'F', 7));
 
         Assert.assertEquals(dimensionDtoExpectedFrom200F7, dimensionDtoImpFrom200F7);
         Assert.assertNotEquals(dimensionDtoExpectedFrom200F7, testClassOfDimensionDtoImp);
--- src/test/java/com/CezaryZal/api/service/calculation/result/ResultForShaftTest.java
+++ src/test/java/com/CezaryZal/api/service/calculation/result/ResultForShaftTest.java
@@ -29,17 +29,15 @@ public class ResultForShaftTest {
     //80j5 - -12 (lower), 1 (upper)
     @Test
     public void calculate_should_testing_for_symbol_over_H() {
-        given(resultForShaft.calculateForSymbolOverH(80, -12, 13))
-                .willReturn(new DimensionDtoImpl(80, -12, 1));
-
-        ParsedInputDimension parsedInputDimensionFrom80j5 =
-                new ParsedInputDimension(80, 'j', 5);
-        ValuesToDimensionDto valuesToDimensionDtoFrom80j5 =
-                new ValuesToDimensionDto(-12, 13);
-        DimensionDto dimensionDtoExpectedFrom80j5 =
-                resultForShaft.calculate(valuesToDimensionDtoFrom80j5, parsedInputDimensionFrom80j5);
-
         DimensionDtoImpl dimensionDtoImpFrom80j5 = new DimensionDtoImpl(80, -12, 1);
+
+        given(resultForShaft.calculateForSymbolOverH(80, -12, 13))
+                .willReturn(dimensionDtoImpFrom80j5);
+
+        DimensionDto dimensionDtoExpectedFrom80j5 =
+                resultForShaft.calculate(
+                        new ValuesToDimensionDto(-12, 13),
+                        new ParsedInputDimension(80, 'j', 5));
 
         Assert.assertEquals(dimensionDtoExpectedFrom80j5, dimensionDtoImpFrom80j5);
         Assert.assertNotEquals(dimensionDtoExpectedFrom80j5, testClassOfDimensionDtoImp);
@@ -49,33 +47,28 @@ public class ResultForShaftTest {
     //40g5 - -20 (lower), -9 (upper)
     @Test
     public void calculate_should_testing_for_symbol_below_or_equal_H() {
-        given(resultForShaft.calculateForSymbolBelowOrEqualH(45, 0, 25))
-                .willReturn(new DimensionDtoImpl(45, -25, 0));
-
-        ParsedInputDimension parsedInputDimensionFrom45h7 =
-                new ParsedInputDimension(45, 'h', 7);
-        ValuesToDimensionDto valuesToDimensionDtoFrom45h7 =
-                new ValuesToDimensionDto(0, 25);
-        DimensionDto dimensionDtoExpectedFrom45h7 =
-                resultForShaft.calculate(valuesToDimensionDtoFrom45h7, parsedInputDimensionFrom45h7);
-
         DimensionDtoImpl dimensionDtoImpFrom45h7 = new DimensionDtoImpl(45, -25, 0);
+        DimensionDtoImpl dimensionDtoImpFrom40g5 = new DimensionDtoImpl(40, -20, -9);
+
+        given(resultForShaft.calculateForSymbolBelowOrEqualH(45, 0, 25))
+                .willReturn(dimensionDtoImpFrom45h7);
+
+        DimensionDto dimensionDtoExpectedFrom45h7 =
+                resultForShaft.calculate(
+                        new ValuesToDimensionDto(0, 25),
+                        new ParsedInputDimension(45, 'h', 7));
 
         Assert.assertEquals(dimensionDtoExpectedFrom45h7, dimensionDtoImpFrom45h7);
         Assert.assertNotEquals(dimensionDtoExpectedFrom45h7, testClassOfDimensionDtoImp);
 
 
         given(resultForShaft.calculateForSymbolBelowOrEqualH(40, -9, 11))
-                .willReturn(new DimensionDtoImpl(40, -20, -9));
+                .willReturn(dimensionDtoImpFrom40g5);
 
-        ParsedInputDimension parsedInputDimensionFrom40g5 =
-                new ParsedInputDimension(40, 'g', 5);
-        ValuesToDimensionDto valuesToDimensionDtoFrom40g5 =
-                new ValuesToDimensionDto(-9, 11);
         DimensionDto dimensionDtoExpectedFrom40g5 =
-                resultForShaft.calculate(valuesToDimensionDtoFrom40g5, parsedInputDimensionFrom40g5);
-
-        DimensionDtoImpl dimensionDtoImpFrom40g5 = new DimensionDtoImpl(40, -20, -9);
+                resultForShaft.calculate(
+                        new ValuesToDimensionDto(-9, 11),
+                        new ParsedInputDimension(40, 'g', 5));
 
         Assert.assertEquals(dimensionDtoExpectedFrom40g5, dimensionDtoImpFrom40g5);
         Assert.assertNotEquals(dimensionDtoExpectedFrom40g5, testClassOfDimensionDtoImp);
