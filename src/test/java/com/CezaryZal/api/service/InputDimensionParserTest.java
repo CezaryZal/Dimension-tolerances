c51aad233ffc19520f19ca7cf3273490c71daf7c
CezaryZal <czarek0821@gmail.com>
2020-03-14 21:04:46 +0100

    Code optimization in InputDimensionParserTest class
    Change methods Mockito library to BDDMockito in class;
    Add private to members in DimensionServiceTest class;
    Add new test cases;

--- src/test/java/com/CezaryZal/api/service/InputDimensionParserTest.java
+++ src/test/java/com/CezaryZal/api/service/InputDimensionParserTest.java
@@ -7,107 +7,135 @@ import com.CezaryZal.exceptions.InvalidSymbolsException;
 import com.CezaryZal.exceptions.InvalidValueOfDimensionException;
 import com.CezaryZal.exceptions.InvalidValueOfItException;
 import lombok.SneakyThrows;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static com.CezaryZal.constant.DescriptionOfConstants.DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.Assert.*;
+
 @RunWith(MockitoJUnitRunner.class)
 public class InputDimensionParserTest {
 
     @Spy
-    InputValidator inputValidator;
+    private InputValidator inputValidator;
 
     @InjectMocks
-    InputDimensionParser inputDimensionParser;
+    private InputDimensionParser inputDimensionParser;
 
     @Test
     public void parse_input_dimension_should_throw_invalid_value_of_dimension_exception() {
-        Assert.assertThrows(
+        assertThrows(
                 InvalidValueOfDimensionException.class, () -> inputDimensionParser.parseInputDimension("0a2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidValueOfDimensionException.class, () -> inputDimensionParser.parseInputDimension("501a2"));
     }
 
     @Test
     public void parse_input_dimension_should_throw_invalid_symbols_exception() {
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1aa2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1t2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1T2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1b2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1i2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1l2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1o2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1q2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1B2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1I2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1L2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1O2"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidSymbolsException.class, () -> inputDimensionParser.parseInputDimension("1Q2"));
     }
 
     @Test
     public void parse_input_dimension_should_throw_invalid_value_of_IT_exception() {
-        Assert.assertThrows(
+        assertThrows(
                 InvalidValueOfItException.class, () -> inputDimensionParser.parseInputDimension("1a0"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidValueOfItException.class, () -> inputDimensionParser.parseInputDimension("1a19"));
     }
 
     @Test
     public void parse_input_dimension_should_throw_invalid_input_exception() {
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension(""));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("4%5E5k7"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("5^1k8"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("50t8k"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("50t8k6"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("58"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("k8"));
-        Assert.assertThrows(
+        assertThrows(
                 InvalidInputException.class, () -> inputDimensionParser.parseInputDimension("80G"));
     }
 
     @SneakyThrows
     @Test
     public void parse_input_dimension_should_not_throw_exceptions() {
-        ParsedInputDimension parsedInputDimensionFrom1s7 = inputDimensionParser.parseInputDimension("1s7");
-        ParsedInputDimension parsedInputDimensionFrom500s7 = inputDimensionParser.parseInputDimension("500s7");
-        ParsedInputDimension parsedInputDimensionFrom50S7 = inputDimensionParser.parseInputDimension("50S7");
-        ParsedInputDimension parsedInputDimensionFrom50S1 = inputDimensionParser.parseInputDimension("50S1");
-        ParsedInputDimension parsedInputDimensionFrom50S18 = inputDimensionParser.parseInputDimension("50S18");
+        ParsedInputDimension parsedInputDimensionFrom1M7 = inputDimensionParser.parseInputDimension("1M7");
+        ParsedInputDimension parsedInputDimensionFrom500m5 = inputDimensionParser.parseInputDimension("500m5");
+        ParsedInputDimension parsedInputDimensionFrom55a3 = inputDimensionParser.parseInputDimension("55a3");
+        ParsedInputDimension parsedInputDimensionFrom72s4 = inputDimensionParser.parseInputDimension("72s4");
+        ParsedInputDimension parsedInputDimensionFrom8A2 = inputDimensionParser.parseInputDimension("8A2");
+        ParsedInputDimension parsedInputDimensionFrom43S4 = inputDimensionParser.parseInputDimension("43S4");
+        ParsedInputDimension parsedInputDimensionFrom22n1 = inputDimensionParser.parseInputDimension("22n1");
+        ParsedInputDimension parsedInputDimensionFrom89K18 = inputDimensionParser.parseInputDimension("89K18");
 
-        ParsedInputDimension expectedParsedInputDimensionFrom1s7 = new ParsedInputDimension(1, 's', 7);
-        ParsedInputDimension expectedParsedInputDimensionFrom500s7 = new ParsedInputDimension(500, 's', 7);
-        ParsedInputDimension expectedParsedInputDimensionFrom50S7 = new ParsedInputDimension(50, 'S', 7);
-        ParsedInputDimension expectedParsedInputDimensionFrom50S1 = new ParsedInputDimension(50, 'S', 1);
-        ParsedInputDimension expectedParsedInputDimensionFrom50S18 = new ParsedInputDimension(50, 'S', 18);
+        ParsedInputDimension expectedParsedInputDimensionFrom1M7 = new ParsedInputDimension(1, 'M', 7);
+        ParsedInputDimension expectedParsedInputDimensionFrom500m5 = new ParsedInputDimension(500, 'm', 5);
+        ParsedInputDimension expectedParsedInputDimensionFrom55a3 = new ParsedInputDimension(55, 'a', 3);
+        ParsedInputDimension expectedParsedInputDimensionFrom72s4 = new ParsedInputDimension(72, 's', 4);
+        ParsedInputDimension expectedParsedInputDimensionFrom8A2 = new ParsedInputDimension(8, 'A', 2);
+        ParsedInputDimension expectedParsedInputDimensionFrom43S4 = new ParsedInputDimension(43, 'S', 4);
+        ParsedInputDimension expectedParsedInputDimensionFrom22n1 = new ParsedInputDimension(22, 'n', 1);
+        ParsedInputDimension expectedParsedInputDimensionFrom89K18 = new ParsedInputDimension(89, 'K', 18);
 
-        Assert.assertEquals(expectedParsedInputDimensionFrom1s7, parsedInputDimensionFrom1s7);
-        Assert.assertEquals(expectedParsedInputDimensionFrom500s7, parsedInputDimensionFrom500s7);
-        Assert.assertEquals(expectedParsedInputDimensionFrom50S7, parsedInputDimensionFrom50S7);
-        Assert.assertEquals(expectedParsedInputDimensionFrom50S1, parsedInputDimensionFrom50S1);
-        Assert.assertEquals(expectedParsedInputDimensionFrom50S18, parsedInputDimensionFrom50S18);
+        then(parsedInputDimensionFrom1M7)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom1M7);
+        then(parsedInputDimensionFrom500m5)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom500m5);
+        then(parsedInputDimensionFrom55a3)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom55a3);
+        then(parsedInputDimensionFrom72s4)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom72s4);
+        then(parsedInputDimensionFrom8A2)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom8A2);
+        then(parsedInputDimensionFrom43S4)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom43S4);
+        then(parsedInputDimensionFrom22n1)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom22n1);
+        then(parsedInputDimensionFrom89K18)
+                .as(DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT)
+                .isEqualTo(expectedParsedInputDimensionFrom89K18);
     }
 }
