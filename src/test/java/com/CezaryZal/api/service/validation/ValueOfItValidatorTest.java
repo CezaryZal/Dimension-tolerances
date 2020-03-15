55a95410f33b18b96baec477ed0ecc4ccdfeb3ee
CezaryZal <czarek0821@gmail.com>
2020-03-15 10:40:13 +0100

    Code optimization and add description to asserts

--- src/test/java/com/CezaryZal/api/service/validation/ValueOfItValidatorTest.java
+++ src/test/java/com/CezaryZal/api/service/validation/ValueOfItValidatorTest.java
@@ -1,32 +1,79 @@
 package com.CezaryZal.api.service.validation;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static com.CezaryZal.constant.DescriptionOfConstants.DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE;
+import static com.CezaryZal.constant.DescriptionOfConstants.DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE;
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ValueOfItValidatorTest {
 
-    ValueOfItValidator valueOfItValidator;
+    @Spy
+    private ValueOfItValidator valueOfItValidator;
 
-    @Before
-    public void setUp() throws Exception {
-        valueOfItValidator = new ValueOfItValidator();
-    }
+    private final String nameOfValueUnit = "IT";
 
     @Test
     public void is_correct_should_be_in_range_of_numbers_1_to_18() {
-        Assert.assertTrue(valueOfItValidator.isCorrect(1));
-        Assert.assertTrue(valueOfItValidator.isCorrect(18));
-        Assert.assertFalse(valueOfItValidator.isCorrect(0));
-        Assert.assertFalse(valueOfItValidator.isCorrect(19));
+        int valueOfIt1 = 1;
+        boolean isCorrectFor1It = valueOfItValidator.isCorrect(valueOfIt1);
+        then(isCorrectFor1It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE + valueOfIt1 + nameOfValueUnit)
+                .isTrue();
+
+        int valueOfIt18 = 18;
+        boolean isCorrectFor18It = valueOfItValidator.isCorrect(valueOfIt18);
+        then(isCorrectFor18It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE + valueOfIt18 + nameOfValueUnit)
+                .isTrue();
     }
 
     @Test
-    public void number_is_correct_should_be_in_range_of_numbers_1_to_500() {
-        Assert.assertTrue(valueOfItValidator.numberIsCorrect(1));
-        Assert.assertTrue(valueOfItValidator.numberIsCorrect(18));
-        Assert.assertFalse(valueOfItValidator.numberIsCorrect(0));
-        Assert.assertFalse(valueOfItValidator.numberIsCorrect(19));
+    public void is_correct_should_not_be_in_range_of_numbers_1_to_18() {
+        int valueOfIt0 = 0;
+        boolean isCorrectFor0It = valueOfItValidator.isCorrect(valueOfIt0);
+        then(isCorrectFor0It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE + valueOfIt0 + nameOfValueUnit)
+                .isFalse();
+
+        int valueOfIt19 = 19;
+        boolean isCorrectFor19It = valueOfItValidator.isCorrect(valueOfIt19);
+        then(isCorrectFor19It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE + valueOfIt19 + nameOfValueUnit)
+                .isFalse();
     }
 
+    @Test
+    public void number_is_correct_should_be_in_range_of_numbers_1_to_18() {
+        int valueOfIt1 = 1;
+        boolean isCorrectFor1It = valueOfItValidator.numberIsCorrect(valueOfIt1);
+        then(isCorrectFor1It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE + valueOfIt1 + nameOfValueUnit)
+                .isTrue();
+
+        int valueOfIt18 = 18;
+        boolean isCorrectFor18It = valueOfItValidator.numberIsCorrect(valueOfIt18);
+        then(isCorrectFor18It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE + valueOfIt18 + nameOfValueUnit)
+                .isTrue();
+    }
+
+    @Test
+    public void number_is_correct_should_not_be_in_range_of_numbers_1_to_18() {
+        int valueOfIt0 = 0;
+        boolean isCorrectFor0It = valueOfItValidator.numberIsCorrect(valueOfIt0);
+        then(isCorrectFor0It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE + valueOfIt0 + nameOfValueUnit)
+                .isFalse();
+
+        int valueOfIt19 = 19;
+        boolean isCorrectFor19It = valueOfItValidator.numberIsCorrect(valueOfIt19);
+        then(isCorrectFor19It)
+                .as(DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE + valueOfIt19 + nameOfValueUnit)
+                .isFalse();
+    }
 }
--- src/test/java/com/CezaryZal/constant/DescriptionOfConstants.java
+++ src/test/java/com/CezaryZal/constant/DescriptionOfConstants.java
@@ -3,7 +3,14 @@ package com.CezaryZal.constant;
 public class DescriptionOfConstants {
 
     public static final String DESCRIPTION_OF_FAIL_FOR_COMPARING_OBJECT =
-            "objects being compared are not equal";
+            "The object obtained is not equal to the expected. The value entered is ";
+
     public static final String DESCRIPTION_OF_FAIL_WHEN_COMPARED_OBJECT_ARE_EQUALS =
-            "objects being compared are equal, but they should not";
+            "The object obtained is equal, but they should not. The value entered is ";
+
+    public static final String DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_TRUE =
+            "The method invoked should be true, the value entered is ";
+
+    public static final String DESCRIPTION_OF_FAIL_WHEN_DO_NOT_GET_FALSE =
+            "The method invoked should be false, the value entered is ";
 }
